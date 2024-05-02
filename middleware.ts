8da09aeca1e442d5eb11b0eb0cb5405e786f9d19
MHABM <mhabm2th@gmail.com>
2024-05-02 21:38:12 +0330

    login undefined2

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
  
-// export default NextAuth(authConfig).auth;
-// export const config = {matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']};
-export const { auth: middleware } = NextAuth(authConfig)
+export default NextAuth(authConfig).auth;
+export const config = {matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']};
